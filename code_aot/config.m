6a3a8467a799693d24b077ed5f11c5d265b81ab4
Erik Nijkamp <erik.nijkamp@gmail.com>
2017-06-29 21:33:13 -0700

    aot config

--- code_aot/config.m
+++ code_aot/config.m
@@ -12,42 +12,6 @@ para.categoryNames = {'BearHead','CatHead','ChickenHead','CowHead', ...
                   'TigerHead','WolfHead'};
         
 
-%% 5 clusters (toy)
-
-if strcmp(para.name, 'cluster_5_toy')
-    
-    para.task_ids = [2];
-
-    para.method = 'two_stage';  % 'two_stage' : matching pursuit learning,  'one_stage': generative boosting learning
-
-    para.nPartCol = 2;
-    para.nPartRow = 2;
-    para.part_sx = 50;
-    para.part_sy = 50;
-
-    para.GaborScaleList = [0.7];
-    para.DoGScaleList = [];
-
-    para.numCluster = 5;
-    para.numWavelet = 300;   % default 300, 370
-    para.numEMIteration = 10;
-    para.isLocalNormalize = true;
-
-    para.relativePartRotationRange=1*(-1:1);
-    para.relativePartLocationRange=1;
-
-    para.resolutionShiftLimit = 1;
-    para.rotateShiftLimit = 3;   % template rotation  from -rotateShiftLimit to rotateShiftLimit, eg. (1)-2:2 if rotateShiftLimit=2 (2)0 is without rotation
-    para.ratioDisplacementSUM3=0;   % default=0. Compute all values in SUM3 map
-
-    para.locationShiftLimit=3;  % arg-max
-    para.orientShiftLimit=1;    % arg-max
-
-    para.numResolution = 3;     
-    
-    para.useSUM3 = 1;
-
-end
 
 %% 5 clusters
 
@@ -86,44 +50,6 @@ if strcmp(para.name, 'cluster_5_full')
     
 end
 
-
-%% 5 cluster (without parts / baseline)
-
-if strcmp(para.name, 'cluster_5_base')
-    
-    para.task_ids = 1:20;
-
-    para.method = 'two_stage';  % 'two_stage' : matching pursuit learning,  'one_stage': generative boosting learning
-
-    para.nPartCol = 2;
-    para.nPartRow = 2;
-    para.part_sx = 50;
-    para.part_sy = 50;
-
-    para.GaborScaleList = [0.7];
-    para.DoGScaleList = [];
-
-    para.numCluster = 5;
-    para.numWavelet = 300;   % default 300, 370
-    para.numEMIteration = 10;
-    para.isLocalNormalize = true;
-
-    para.relativePartRotationRange=0*(-1:1);
-    para.relativePartLocationRange=0;
-
-    para.resolutionShiftLimit = 0;
-    para.rotateShiftLimit = 3;   % template rotation  from -rotateShiftLimit to rotateShiftLimit, eg. (1)-2:2 if rotateShiftLimit=2 (2)0 is without rotation
-    para.ratioDisplacementSUM3=0;   % default=0. Compute all values in SUM3 map
-
-    para.locationShiftLimit=3;  % arg-max
-    para.orientShiftLimit=1;    % arg-max
-
-    para.numResolution = 3;    
-    
-    para.useSUM3 = 1;
-
-end
-
 %% 11 cluster
 
 if strcmp(para.name, 'cluster_11_full')
@@ -149,43 +75,6 @@ if strcmp(para.name, 'cluster_11_full')
     para.relativePartLocationRange=1;
 
     para.resolutionShiftLimit = 1;
-    para.rotateShiftLimit = 3;   % template rotation  from -rotateShiftLimit to rotateShiftLimit, eg. (1)-2:2 if rotateShiftLimit=2 (2)0 is without rotation
-    para.ratioDisplacementSUM3=0;   % default=0. Compute all values in SUM3 map
-
-    para.locationShiftLimit=3;  % arg-max
-    para.orientShiftLimit=1;    % arg-max
-
-    para.numResolution = 3; 
-    
-    para.useSUM3 = 1;
-
-end
-
-%% 11 cluster (without parts / baseline)
-
-if strcmp(para.name, 'cluster_11_base')
-    
-    para.task_ids = 1:20;
-
-    para.method = 'two_stage';  % 'two_stage' : matching pursuit learning,  'one_stage': generative boosting learning
-
-    para.nPartCol = 2;
-    para.nPartRow = 2;
-    para.part_sx = 50;
-    para.part_sy = 50;
-
-    para.GaborScaleList = [0.7];
-    para.DoGScaleList = [];
-
-    para.numCluster = 11;
-    para.numWavelet = 300;   % default 300, 370
-    para.numEMIteration = 10;
-    para.isLocalNormalize = true;
-
-    para.relativePartRotationRange=0*(-1:1);
-    para.relativePartLocationRange=0;
-
-    para.resolutionShiftLimit = 0;
     para.rotateShiftLimit = 3;   % template rotation  from -rotateShiftLimit to rotateShiftLimit, eg. (1)-2:2 if rotateShiftLimit=2 (2)0 is without rotation
     para.ratioDisplacementSUM3=0;   % default=0. Compute all values in SUM3 map
 
